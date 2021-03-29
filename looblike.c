051268dbdd7cf4bd663c7d8d12e411162bcdbd95
b6320502444 <preechaya.chi@ku.th>
2021-03-29 19:33:26 +0700

    ใ

--- looblike.c
+++ looblike.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,max=0;
+    int n,i,y=0,max=0;
     scanf("%d",&n);
-    int a[n],x[n];
+    int a[n],x=0;
     for(i=0;i<n;i++)
     {
-        x[i]=0;
         scanf("%d",&a[i]);
     }
     for(i=0;i<n;i++)
@@ -14,17 +13,17 @@ int main()
         if(max<a[i])
         {
             max=a[i];
-            x[n]=i+1;
+            x=i+1;
         }
     }
     for(i=0;i<n;i++)
     {
-            if(max==a[i])
-            {
-                max=a[i];
-                x[n]=i+1;
-            }
+        if(max==a[i])
+        {
+            max=a[i];
+            x=i+1;
+            printf("%d ",x);
+        }
     }
-    printf("%d",x[n]);
 return 0;
 }
